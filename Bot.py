037bf5fc78d9abae4268471939ce1a753a006eb2
Pablo3675 <146583366+Pablo3675@users.noreply.github.com>
2023-11-18 03:26:08 -0500

    Update Bot.py
    Agregue un comando para ver la información de un usuario y ahora el comando de memes da imágenes al azar

--- Bot.py
+++ Bot.py
@@ -1,4 +1,5 @@
 import discord
+import os 
 import random
 from discord.ext import commands
 
@@ -10,6 +11,10 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def Help(ctx):
+    await ctx.send("Mi lista de conandos es:\n1- $hello\n2- $heh\n3- $coin\n4- $userinfo\n5- $mem")
 
 @bot.command()
 async def hello(ctx):
@@ -24,8 +29,18 @@ async def coin(ctx, coin_game = ["cara", "sello"]):
     await ctx.send(random.choice(coin_game))
 
 @bot.command()
+async def userinfo(ctx: commands.Context, user: discord.User):
+    user_id = user.id
+    username = user.name
+    avatar = user.display_avatar.url
+    await ctx.send(f'User found: {user_id} -- {username}\n{avatar}')
+
+@bot.command()
 async def mem(ctx):
-    with open('Kodland Python\Memes\Meme1.jpg', 'rb') as f:
+    meme_carpeta = "Kodland Python\Memes"
+    meme_archivos = [f for f in os.listdir(meme_carpeta)]
+    meme_random = os.path.join(meme_carpeta, random.choice(meme_archivos))
+    with open(meme_random, 'rb') as f:
         # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
         picture = discord.File(f)
     # A continuación, podemos enviar este archivo como parámetro.
